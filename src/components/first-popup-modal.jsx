a18a87d1448f738d75a4e0b35fc29a1351ab05ca
0dot77 <yty0706@gmail.com>
2022-08-04 15:12:59 +0900

    add: first-toggle

--- src/components/first-popup-modal.jsx
+++ src/components/first-popup-modal.jsx
@@ -6,27 +6,66 @@ const MainContainer = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
 
+const ImgContainer = styled.div`
+  width: 500px;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  overflow-y: hidden;
   img {
     width: 500px;
     height: 500px;
-    overflow-y: hidden;
-    margin: 0;
+    z-index: 1000;
   }
+`;
+
+const CloseContainer = styled.div`
+  width: 500px;
+  height: 620px;
+  position: absolute;
+  display: flex;
+  justify-content: end;
+  z-index: 2000;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
+const Close = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: red;
 `;
 
 //TODO: location을 확인해서 각 로케에 맞는 이미지로 바꾸기
 export default function FirstPopUpModal({ clickNum, setClickNum }) {
   return (
     <MainContainer>
-      <img
-        src={`/assets/first/${clickNum}.jpeg`}
-        onClick={() => {
-          if (clickNum < 6) {
-            setClickNum(clickNum + 1);
-          }
-        }}
-      />
+      <ImgContainer>
+        <img src={`/assets/first/${clickNum}.jpeg`} />
+      </ImgContainer>
+      <CloseContainer>
+        <Close
+          onClick={() => {
+            if (clickNum < 6) {
+              setClickNum(clickNum + 1);
+            }
+          }}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path
+              fill-rule="evenodd"
+              fill="white"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </Close>
+      </CloseContainer>
     </MainContainer>
   );
 }
--- src/pages/First.jsx
+++ src/pages/First.jsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import FirstPopUpModal from '../components/first-popup-modal';
-import { windowPopUpSize } from '../db';
 
 //TODO: 1. 페이지에 접근하면, 팝업이 1초 뒤에 뜨도록
 
